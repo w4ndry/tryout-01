da1dd355fe3b24953f0f94f64c7bc82aa3bb4069
w4ndry <eriwandri@gmail.com>
2017-04-01 15:01:08 +0700

    Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tryout-01
+
+a.  Repo Name: https://github.com/w4ndry/tryout-01
+b.  Author: Eri Wandri
+c.  Contact: eriwandri@gmail.com
+d.  Lisense: MIT
+e.  To run server 'npm start'
--- graphql/ApolloClient/app/App.js
+++ graphql/ApolloClient/app/App.js
@@ -1,46 +1,83 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { graphql, ApolloProvider } from 'react-apollo';
-import gql from 'graphql-tag';
+import React, { Component } from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag'
 
-const styles = {
-  outer: { paddingTop: 32, paddingLeft: 10, paddingRight: 10 },
-  wrapper: { height: 40, marginBottom: 15, flex: 1, flexDirection: 'row' },
-  header: { fontSize: 20 },
-  subtextWrapper: { flex: 1, flexDirection: 'row' },
-  votes: { color: '#999' },
-}
-
-// The data prop, which is provided by the wrapper below contains,
-// a `loading` key while the query is in flight and posts when ready
-function PostList({ data: { loading, channels } }) {
-  if (loading) {
-    return <Text style={styles.outer}>Loading</Text>;
-  } else {
-    return (
-      <View style={styles.outer}>
-        {channels.map(post => (
-          <View key={post.id} style={styles.wrapper}>
-            <View>
-              <Text style={styles.header}>{post.id}</Text>
-              <Text style={styles.header}>{post.name}</Text>
-              <Text style={styles.header}>{post.address}</Text>              
-            </View>
-          </View>
-        ))}
-      </View>
-    );
-  }
-}
-
-// The `graphql` wrapper executes a GraphQL query and makes the results
-// available on the `data` prop of the wrapped component (PostList here)
-export default graphql(gql`
-  query allPosts {
-    channels {
-      id
-      name
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+			id: '',
+      name: '',
       address
     }
+    this.updateName = this.updateName.bind(this)
   }
-`)(PostList);
+  updateName(name) {
+    this.setState({
+      name
+    })
+  }
+  render () {
+		const networkInterface = createNetworkInterface({ 
+  uri: 'http://localhost:3000/graphql',
+});
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const channelsListQuery = gql`
+   query ChannelsListQuery {
+     channels {
+       id
+       name
+       address
+     }
+   }
+ `;
+		
+		{/*const query = gql`query PresidentQuery($name: String!) { 
+      president(name: $name) {
+        name
+        term
+        party
+      }
+    }`}
+
+    const Channels = ({ data }) => (
+      <View style={{paddingLeft: 20, paddingTop: 20}}>
+        <Text>Id: {data.channels && data.channels.id}</Text>
+        <Text>Name: {data.channels && data.channels.name}</Text>
+      </View>
+    )
+    
+    const ViewWithData = graphql(query, {
+      options: { variables: { name: this.state.name } }
+    })(Channels)*/}
+
+    return (
+      <View style={styles.container}>
+        <Text style={{textAlign: 'center'}}>Find Name</Text>
+				<Text>Id: {this.state.id}</Text>
+				<Text>Name: {this.state.name}</Text>
+      </View>
+    )
+  }
+}
+
+styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: '#dddddd',
+    height: 50,
+    margin: 20,
+    marginBottom: 0,
+    paddingLeft: 10
+  }
+})
+
+export default App
